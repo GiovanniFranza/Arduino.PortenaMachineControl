f9e6ae8e5ba5b59044f9df1575dbac0921ae5c43
GiovanniFranza <franzagiovanni95@gmail.com>
2022-10-26 13:09:51 +0200

    Update LED.ino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,13 +1,15 @@
-using namespace machinecontrol;
 
 #include <Wire.h>
 #include <Arduino_MachineControl.h>
+using namespace machinecontrol;
 
 int puls=DIN_READ_CH_PIN_00;
+int led=0;
 void setup() 
 {
   Wire.begin();
   Serial.begin(9600);
+  digital_outputs.setAll(0);
 
   if (!digital_inputs.init()) {
     Serial.println("Digital input GPIO expander initialization fail!!");
@@ -16,6 +18,13 @@ void setup()
 
 void loop() 
 {
+  if(digital_inputs.read(puls)==1)
+  {
+    digital_outputs.set(led,HIGH);
+  }
+  else
+  {
+    digital_outputs.set(led,LOW);
 
-  Serial.println("Hello World");
+  }
 }
