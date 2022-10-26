74536b5d68c180f968fd4eb099d104aacf951f53
GiovanniFranza <franzagiovanni95@gmail.com>
2022-10-26 16:43:07 +0200

    Update LED.ino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,40 +1,28 @@
 #include <Wire.h>
 #include <Arduino_MachineControl.h>
-
 using namespace machinecontrol;
 
-int pulsAvvio=DIN_READ_CH_PIN_00;
-int ftcAvanti=DIN_READ_CH_PIN_01;
-int ftcIndietro=DIN_READ_CH_PIN_02;
-int motoreAvanti=0;
-int motoreIndietro=0;
-
-
+int puls=DIN_READ_CH_PIN_00;
+int led=0;
 void setup() 
 {
   Wire.begin();
   Serial.begin(9600);
-
   digital_outputs.setAll(0);
 
-  digital_outputs.setAll(motoreAvanti,LOW);
-  digital_outputs.setAll(motoreIndietro,LOW);
-
-  if (!digital_inputs.init()) 
-  {
+  if (!digital_inputs.init()) {
     Serial.println("Digital input GPIO expander initialization fail!!");
-  }
-}
 
 void loop() 
 {
-}
-
-void Marcia()
-{
-  int valorePuls=digital_inputs_read(pulsAvvio);
-  if(valorePuls==1 && ftcAvanti!=1)
+  if(digital_inputs.read(puls)==1)
   {
-    digital_outputs.set(motoreAvanti,HIGH);
+    digital_outputs.set(led,HIGH);
+  }
+  else
+  {
+    digital_outputs.set(led,LOW);
+
+  Serial.println("Hello World");
   }
 }
