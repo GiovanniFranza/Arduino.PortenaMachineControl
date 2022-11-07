4e655be95f777948d31dbd20bca8d2d07d930e18
GiovanniFranza <franzagiovanni95@gmail.com>
2022-11-07 12:35:28 +0100

    Update folder

--- Read Analog/Read Analog.ino
+++ Read Analog/Read Analog.ino
@@ -3,29 +3,29 @@
 using namespace machinecontrol;
 
 //float res_divider = 0.28057;
-float res_divider = 1.5;
-float reference = 3.3;
-int motore=0;
-int ledRosso=1;
-int ledVerde=2;
-int ledGiallo=3;
+
+float res_divider;
+float reference;
+int motore;
 int encoder=DIN_READ_CH_PIN_00;
-int contatore=0;
-int var1=0;
-int var2=0;
-int var3=0;
+bool abilitazione;
+float sogliaRiferimento;
+float valMinimo;
 
 void setup()
 {
+  valMinimo=100;
+  sogliaRiferimento=1.49;
+  abilitazione=false;
+  motore=0;
+  reference=3.3;
+  res_divider=1.5;
   analogReadResolution(16);
   Serial.begin(9600);
   Wire.begin();
   analog_in.set0_10V();
   digital_outputs.setAll(0);
   digital_outputs.set(motore,LOW);
-  digital_outputs.set(ledRosso,LOW);
-  digital_outputs.set(ledVerde,LOW);
-  digital_outputs.set(ledGiallo,LOW);
 
   if (!digital_inputs.init()) 
   {
@@ -35,17 +35,14 @@ void setup()
 
 void loop() 
 {
-  bool abilitazione=false;
-  float sogliaRiferimento=1.49;
-  int valMinimo=100;
-
   int letturaEncoder=digital_inputs.read(encoder);
   digital_outputs.set(motore,HIGH);
   float raw_voltage_ch0 = analog_in.read(0);
   float voltage_ch0 = (raw_voltage_ch0 * reference) / 65535 / res_divider;
   delay(100);
   
-  if(voltage_ch0 < sogliaRiferimento && sogliaRiferimento >=1.49 && sogliaRiferimento<=1.50)
+  
+  if(!(voltage_ch0 >= 1.46 &&  voltage_ch0 <= 1.53))
   {
     abilitazione=true;
   }else
@@ -55,60 +52,39 @@ void loop()
 
   if(abilitazione)
   {
-    if(voltage_ch0!=valMinimo)
+    if(voltage_ch0<=valMinimo)
     {
       valMinimo=voltage_ch0;
     }
   }
 
-  if(valMinimo!=100 & abilitazione==true)
+  if(valMinimo!=100 & abilitazione==false)
   {
-    if(voltage_ch0>=1.25 && voltage_ch0<=1.30) 
-    {
-      var1=1;
-    }
-    if(var1==1)
+    if(valMinimo>=1.25 && valMinimo<=1.30) 
     {
       Serial.println("Rosso");
-      delay(1000);
-      Serial.println("------");
-      var1=0;
     }
     else
     {    
       Serial.println("------");
     }
-    if(voltage_ch0>=0.7 && voltage_ch0<=0.8)
-    {
-      var2=1;
-    }
-    if(var2==1)
+    if(valMinimo>=0.7 && valMinimo<=0.8)
     {
       Serial.println("Bianco");
-      delay(1000);
-      Serial.println("------");
-      var2=0;
     }
     else
     {    
       Serial.println("------");
     }
 
-    if(voltage_ch0>=1.38 && voltage_ch0<=1.42)
-    {
-      var3=1;
-    }
-    if(var3==1)
+    if(valMinimo>=1.38 && valMinimo<=1.42)
     {
       Serial.println("Blu");
-      delay(1000);
-      Serial.println("------");
-      var3=0;
     }
     else
     {    
       Serial.println("------");
     }
+    valMinimo=100;
   }
-  valMinimo=100;
 }
