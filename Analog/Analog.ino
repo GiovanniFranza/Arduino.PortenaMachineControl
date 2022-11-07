ae15e708eb4d186c7b032ab7706919cd0d936df4
GiovanniFranza <franzagiovanni95@gmail.com>
2022-11-07 11:28:06 +0100

    Update Analog.ino

--- Analog/Analog.ino
+++ Analog/Analog.ino
@@ -5,9 +5,6 @@ using namespace machinecontrol;
 //float res_divider = 0.28057;
 float res_divider = 1.5;
 float reference = 3.3;
-const float sogliaRiferimento=1.49;
-bool abilitazione=false;
-int valMinimo=100;
 int motore=0;
 int ledRosso=1;
 int ledVerde=2;
@@ -38,14 +35,17 @@ void setup()
 
 void loop() 
 {
+  bool abilitazione=false;
+  float sogliaRiferimento=1.49;
+  int valMinimo=100;
+
   int letturaEncoder=digital_inputs.read(encoder);
   digital_outputs.set(motore,HIGH);
   float raw_voltage_ch0 = analog_in.read(0);
   float voltage_ch0 = (raw_voltage_ch0 * reference) / 65535 / res_divider;
-  //Serial.println(voltage_ch0);
   delay(100);
   
-  if(voltage_ch0 < sogliaRiferimento)
+  if(voltage_ch0 < sogliaRiferimento && sogliaRiferimento >=1.49 && sogliaRiferimento<=1.50)
   {
     abilitazione=true;
   }else
@@ -61,7 +61,7 @@ void loop()
     }
   }
 
-  if(valMinimo!=100 & abilitazione==false)
+  if(valMinimo!=100 & abilitazione==true)
   {
     if(voltage_ch0>=1.25 && voltage_ch0<=1.30) 
     {
@@ -110,6 +110,7 @@ void loop()
       Serial.println("------");
     }
   }
+  valMinimo=100;
 }
 
 
